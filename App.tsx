cd9f0b5cdbc6e8e87434dd5fc19af2020d3260a8
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-27 16:21:10 -0300

    estilos eliminados de App.tsx

--- App.tsx
+++ App.tsx
@@ -1,28 +1,18 @@
 import React from "react";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 
 import Routes from "./src/routes";
 
 function App(): JSX.Element {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
+      <SafeAreaView style={{ flex: 1 }}>
         <Routes />
         <StatusBar style="auto" />
       </SafeAreaView>
     </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 export default App;
