e7d4450366279f81485d1bd2308822d58823e668
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-27 16:33:58 -0300

    estructurando Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,31 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+
+const staticInfo = {
+  name: "Emmanuel Cruz",
+  uri: "https://github.com/emmanuel-cruz-dev.png",
+};
 
 const Header = () => {
   return (
-    <View>
-      <Text>Header</Text>
+    <View style={styles.container}>
+      <View style={styles.leftContainer}>
+        <Text style={styles.name}>{staticInfo.name}</Text>
+        <Text>Header</Text>
+      </View>
+      <View style={styles.rightContainer}>
+        <Image source={{ uri: staticInfo.uri }} style={styles.image} />
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+  leftContainer: {},
+  rightContainer: {},
+  name: {},
+  image: {},
+});
 
 export default Header;
