087a02d1f7d458e01aaec9988d893e6314626f69
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-28 11:58:56 -0300

    nombres de Routes e index pasados a tsx

--- App.tsx
+++ App.tsx
@@ -1,19 +1,15 @@
 import React from "react";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 
 import Routes from "./src/routes/Routes";
-import Home from "./src/views/Home";
 
 function App(): JSX.Element {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-        <Routes />
-        {/* <Home /> */}
-        <StatusBar style="auto" />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Routes />
+      <StatusBar style="auto" />
+    </SafeAreaView>
   );
 }
 
--- src/views/AddFood/AddFood.tsx
+++ src/views/AddFood/AddFood.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 import Header from "../../components/Header";
 import { Button, Icon } from "@rneui/base";
+import { Input } from "@rneui/themed";
 
 const AddFood = () => {
   return (
@@ -9,7 +10,7 @@ const AddFood = () => {
       <Header />
       <View style={styles.addFoodContainer}>
         <View style={styles.legendContainer}>
-          <Text>Add food</Text>
+          <Text style={styles.addFoodLegend}>Add food</Text>
         </View>
         <View style={styles.addFoodButtonContainer}>
           <Button
@@ -19,7 +20,17 @@ const AddFood = () => {
           ></Button>
         </View>
       </View>
-      <View></View>
+      <View style={styles.searchContainer}>
+        <View style={styles.inputContainer}>
+          <Input placeholder="Apples, pie, soda..." />
+        </View>
+        <Button
+          title="Search"
+          color={"#ade8af"}
+          titleStyle={styles.searchButtonTitle}
+          radius={"lg"}
+        />
+      </View>
     </View>
   );
 };
@@ -39,6 +50,20 @@ const styles = StyleSheet.create({
   addFoodButtonContainer: {
     flex: 1,
     alignItems: "flex-end",
+  },
+  addFoodLegend: {
+    fontSize: 20,
+  },
+  searchContainer: {
+    flexDirection: "row",
+  },
+  inputContainer: {
+    flex: 1,
+    marginLeft: -12,
+  },
+  searchButtonTitle: {
+    color: "#000",
+    fontSize: 14,
   },
 });
 
