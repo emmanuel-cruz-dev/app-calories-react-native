d19e102710672d80e37f3d958e68d8d9fc81c01e
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-28 16:10:48 -0300

    logica para manejar current data en useFoodStorage

---
+++ src/hooks/useFoodStorage.ts
@@ -0,0 +1,41 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Meal } from "../types/index";
+
+const MY_FOOD_KEY = "@MyFood:Key";
+
+const useFoodStorage = () => {
+  const handleSaveFood = async ({ name, calories, portion }: Meal) => {
+    try {
+      const currentSavedFood = await AsyncStorage.getItem(MY_FOOD_KEY);
+
+      if (currentSavedFood !== null) {
+        const currentSavedFoodParsed = JSON.parse(currentSavedFood);
+        currentSavedFoodParsed.push({
+          name,
+          calories,
+          portion,
+        });
+        await AsyncStorage.setItem(
+          MY_FOOD_KEY,
+          JSON.stringify(currentSavedFoodParsed)
+        );
+      }
+    } catch (error) {}
+  };
+
+  const handleGetFood = () => {
+    AsyncStorage.getItem("food").then((value) => {
+      console.log(value);
+    });
+  };
+
+  return {
+    onSaveFood: handleSaveFood,
+    onGetFood: handleGetFood,
+  };
+};
+
+// Guardar información de los alimentos
+// Método para obtener info de los alimentos
+
+export default useFoodStorage;
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,9 @@ export type RootStackParamList = {
   Home: undefined;
   AddFood: undefined;
 };
+
+export type Meal = {
+  calories: string;
+  name: string;
+  portion: string;
+};
