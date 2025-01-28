15baed13b771bc7a56549ea378a0e023686bdfbf
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-28 14:12:02 -0300

    estilos en Modal boton close

--- src/components/AddFoodModal/AddFoodModal.tsx
+++ src/components/AddFoodModal/AddFoodModal.tsx
@@ -9,10 +9,21 @@ type AddFoodModalProps = {
 
 const AddFoodModal: FC<AddFoodModalProps> = ({ onClose, visible }) => {
   return (
-    <Modal visible={visible} onRequestClose={onClose} transparent>
+    <Modal
+      visible={visible}
+      onRequestClose={onClose}
+      transparent
+      animationType={"slide"}
+    >
       <View style={styles.container}>
         <View style={styles.content}>
-          <Button icon={<Icon name="close" size={24} />} onPress={onClose} />
+          <View style={styles.closeContainer}>
+            <Button
+              icon={<Icon name="close" size={24} />}
+              onPress={onClose}
+              type="clear"
+            />
+          </View>
         </View>
       </View>
     </Modal>
@@ -28,6 +39,20 @@ const styles = StyleSheet.create({
   },
   content: {
     width: "75%",
+    backgroundColor: "#fff",
+    padding: 18,
+    borderRadius: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  closeContainer: {
+    alignItems: "flex-end",
   },
 });
 
