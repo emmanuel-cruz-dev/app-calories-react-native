3ede2b5fa79822f06e6133856cf5973337df3662
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-28 08:20:25 -0300

    agregando button con icon interno y agregando estilos Home.tsx

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { Button } from "@rneui/themed";
+import { Button, Icon } from "@rneui/themed";
 
 import Header from "../../components/Header";
 
@@ -8,12 +8,16 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <Header />
-      <View>
+      <View style={styles.caloriesContainer}>
         <View style={styles.leftContainer}>
           <Text>Calories</Text>
         </View>
         <View style={styles.rightContainer}>
-          <Button>Add Food</Button>
+          <Button
+            radius={"xl"}
+            type="solid"
+            icon={<Icon name="add-circle-outline" color={"#fff"} />}
+          ></Button>
         </View>
       </View>
     </View>
@@ -25,6 +29,11 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 12,
     backgroundColor: "#fff",
+  },
+  caloriesContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 24,
   },
   leftContainer: {
     flex: 1,
