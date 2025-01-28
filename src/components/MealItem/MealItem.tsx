86502df02a89452eb7ac801e3162eddc9ad31699
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-28 17:04:52 -0300

    esqueleto y styles de Meal Item

--- src/components/MealItem/MealItem.tsx
+++ src/components/MealItem/MealItem.tsx
@@ -3,12 +3,17 @@ import { View, Text, StyleSheet } from "react-native";
 
 const MealItem = () => {
   return (
-    <View>
-      <Text>MealItem</Text>
+    <View style={styles.container}>
+      <View style={styles.leftContainer}></View>
+      <View style={styles.rightContainer}></View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+  leftContainer: {},
+  rightContainer: {},
+});
 
 export default MealItem;
