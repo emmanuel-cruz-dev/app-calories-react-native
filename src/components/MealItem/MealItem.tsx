7c948c3fb942827cdb0e3eab06a2908f71bb3352
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-28 18:21:25 -0300

    elementos de Meal Item y agregado de clases

--- src/components/MealItem/MealItem.tsx
+++ src/components/MealItem/MealItem.tsx
@@ -2,12 +2,22 @@ import React, { FC } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 import { Meal } from "../../types";
+import { Button, Icon } from "@rneui/themed";
 
 const MealItem: FC<Meal> = ({ calories, name, portion }) => {
   return (
     <View style={styles.container}>
-      <View style={styles.leftContainer}></View>
-      <View style={styles.rightContainer}></View>
+      <View style={styles.leftContainer}>
+        <Text style={styles.name}>{name}</Text>
+        <Text style={styles.portion}>{portion}</Text>
+      </View>
+      <View style={styles.rightContainer}>
+        <Button
+          icon={<Icon name="add-circle-outline" size={24} />}
+          type="clear"
+        />
+        <Text style={styles.calories}>{calories}</Text>
+      </View>
     </View>
   );
 };
@@ -16,6 +26,9 @@ const styles = StyleSheet.create({
   container: {},
   leftContainer: {},
   rightContainer: {},
+  name: {},
+  portion: {},
+  calories: {},
 });
 
 export default MealItem;
